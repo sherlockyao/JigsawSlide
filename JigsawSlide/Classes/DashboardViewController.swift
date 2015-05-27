8369996c983029bad2e27de5727d0370194a2993
Sherlock Yao <yaosherlock@gmail.com>
2015-05-27 16:19:54 +0800

    add Dashboard view controller

---
+++ JigsawSlide/Classes/DashboardViewController.swift
@@ -0,0 +1,13 @@
+//
+//  JSDashboardViewController.swift
+//  JigsawSlide
+//
+//  Created by Sherlock Yao on 5/27/15.
+//  Copyright (c) 2015 Sherlock Yao. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+    
+}
